549a2062b2683cfcf4c53dca831344c56e6bf3cc
Александр Рязанов < >
2016-11-22 22:37:38 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-function hello(name) {
-  console.log(`Hello ${name}`); // eslint-disable-line
-}
-hello('JS World');
+ import express from 'express';
+ import canonize from './canonizen';
+ const app = express();
+
+ app.get('/', function(req, res){
+ var sname = canonizen(req.query.surname);
+ res.json({
+ sname: req.query.url,
+ surname,
+ });
+ });
+
+ app.listen(3000, () => {
+   console.log('Example app listening on port 3000!');
+ });
